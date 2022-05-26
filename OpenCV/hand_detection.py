44b91353f3b594b209d61026edfc28b7d5153840
felipeflourwears <A01274880@itesm.mx>
2022-05-26 13:24:05 -0500

    Hand Detection Points

--- OpenCV/hand_detection.py
+++ OpenCV/hand_detection.py
@@ -16,7 +16,7 @@ while True:
     
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms)
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
 
     cv2.imshow("Image", img)
