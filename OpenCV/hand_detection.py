65c0bf0c1632514be1ba04e47cb71379d0aa4ec7
felipeflourwears <A01274880@itesm.mx>
2022-05-26 01:35:52 -0500

    Hand Detection

---
+++ OpenCV/hand_detection.py
@@ -0,0 +1,23 @@
+import cv2
+from cv2 import COLOR_BGR2RGB
+import mediapipe as mp
+import time
+
+cap=cv2.VideoCapture(0)
+mpHands=mp.solutions.hands
+hands=mpHands.Hands()
+mpDraw=mp.solutions.drawing_utils
+
+while True:
+    success, img=cap.read()
+    imgRGB=cv2.cvtColor(img, COLOR_BGR2RGB)
+    results=hands.process(imgRGB)
+    #print(results.multi_hand_landmarks)
+    
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms)
+
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
